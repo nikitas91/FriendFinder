4e29def4df6d84c9cb99d3a6e0c68681fa6d0149
Nikita Sasunkevich <nikita@Nikitas-MacBook-Pro.local>
2018-02-09 23:32:53 -0700

    updated routes, added post request to survey, completed server.js

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const friends = require("../data/friends");
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        let userObject = req.body;
+        let userSurveyResponses = userObject.scores;
+
+        let userMatchName = "";
+        let userMathImage = "";
+        let lowestDifference;
+
+        for (let i = 0; i < friends.length; i++) {
+            let computedDifference = 0;
+
+            for (let j = 0; j < userSurveyResponses.length; j++) {
+                computedDifference += Math.abs(friends[i].scores[j] - userSurveyResponses[j]);
+            }
+
+            if (!lowestDifference || (computedDifference < lowestDifference)) {
+                lowestDifference = computedDifference;
+                userMatchName = friends[i].name;
+                userMathImage = friends[i].photo;
+            }
+        }
+
+        friends.push(userObject);
+
+        res.json({ matchName: userMatchName, matchImage: userMathImage });
+    });
+};
